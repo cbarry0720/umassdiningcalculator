15f96c48db62b0c321d461be34ad22d671149088
cbarry0720 <chrisb0720@gmail.com>
2022-11-08 15:39:22 -0500

    track amount of visits

--- backend/Python-flask-with-uwsgi-and-nginx/app/app.py
+++ backend/Python-flask-with-uwsgi-and-nginx/app/app.py
@@ -66,6 +66,19 @@ def foodCalc():
         return "NOT FOUND"
     locs = []
     date = datetime.datetime.now().date().__str__()
+
+    visits = open("visits.json", "r")
+    data = json.load(visits)
+    visits.close()
+
+    visits = open("visits.json", "w")
+    if (date in data):
+        data[date] = data[date] + 1
+    else:
+        data[date] = 1
+    json.dump(data, visits)
+    visits.close()
+
     if "Berkshire Dining Commons" == args["loc"]:
         f = open("berk.json")
         data = json.load(f)
